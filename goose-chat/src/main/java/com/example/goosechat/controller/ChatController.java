78a8a426ee0f81d8e9978b512ef61562d947fb11
Josh Rogers <60600130+trcon@users.noreply.github.com>
2023-02-18 18:41:33 +0000

    Finish starting work?

--- goose-chat/src/main/java/com/example/goosechat/controller/ChatController.java
+++ goose-chat/src/main/java/com/example/goosechat/controller/ChatController.java
@@ -4,11 +4,14 @@ import com.example.goosechat.model.Message;
 import com.example.goosechat.service.ChatroomService;
 import com.example.goosechat.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller public class ChatController {
     @Autowired private SimpMessagingTemplate template;
@@ -21,5 +24,15 @@ import org.springframework.stereotype.Controller;
         message.setChatroom(chatId.get());
 
         Message saved = messageService.save(message);
+    }
+
+    @GetMapping("/messages/{sender}/{recipient}")
+    public ResponseEntity<?> findMessages(@PathVariable String sender, @PathVariable String recipient) {
+        return ResponseEntity.ok(messageService.findChatMessages(sender, recipient));
+    }
+
+    @GetMapping("/messages/{id}")
+    public ResponseEntity<?> findMessage(@PathVariable String id) {
+        return ResponseEntity.ok(messageService.findById(id));
     }
 }
