d0e33d7f84cad71a0d926ac4c895ee7a139f0a39
marwanosman505 <marwanosman505@gmail.com>
2023-02-19 10:40:05 +0000

    final

--- website/goose-messenger/src/components/login/Login.jsx
+++ website/goose-messenger/src/components/login/Login.jsx
@@ -1,8 +1,34 @@
 import React from "react";
+import "./login.css"
+import {Link} from "react-router-dom"
+
 
 const Login = () => {
     return(
-        <h1>Login</h1>
+        <div id="login-form-wrap">
+        <h2>Login</h2>
+        <form id="login-form">
+            <p>
+            <input type="text" id="username" name="username" placeholder="Username/Email Address" required></input>
+            </p>
+
+            <p>
+            <input type="password" id="password" name="password" placeholder="Password" required></input>
+            </p>
+
+
+
+            <p>
+            <Link to="/"><input type="submit" id="login" value="Login"/></Link>
+            </p>
+        </form>
+        <div id="create-account-wrap">
+            <p>Not a member? <Link to="/register">Sign Up</Link></p>
+        </div>
+        </div>
+        
+  
+
     )
 }
 
--- website/goose-messenger/src/components/register/Register.jsx
+++ website/goose-messenger/src/components/register/Register.jsx
@@ -1,8 +1,32 @@
 import React from "react";
+import {Link} from "react-router-dom"
 
 const Register = () => {
     return(
-        <h1>Register</h1>
+        <div id="login-form-wrap">
+        <h2>Signup</h2>
+        <form id="login-form">
+            <p>
+                <input type="text" id="username" name="username" placeholder="Username" required></input>
+            </p>
+
+            <p>
+                <input type="password" id="password" name="password" placeholder="Password" required></input>
+            </p>
+            
+            <p>
+                <input type="conpassword" id="conpassword" name="conpassword" placeholder="Confirm Password" required></input>
+            </p>
+
+            <p>
+                <Link to="/login"><input type="submit" id="login" value="SignUp"/></Link>
+            </p>
+
+        </form>
+        <div id="create-account-wrap">
+            <p>Already a member? <Link to="/login">Login</Link></p>
+        </div>
+    </div>
     )
 }
 
